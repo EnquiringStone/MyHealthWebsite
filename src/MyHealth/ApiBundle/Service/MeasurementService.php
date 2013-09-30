a52b2df03b852c95cde9f70f9ccfd17fbf589c03
jlanges <johan-langes@hotmail.com>
2013-09-30 14:27:38 +0200

    Added measurement service

--- src/MyHealth/ApiBundle/Service/MeasurementService.php
+++ src/MyHealth/ApiBundle/Service/MeasurementService.php
@@ -5,20 +5,11 @@ namespace MyHealth\ApiBundle\Service;
 use Doctrine\ORM\EntityManager;
 use FOS\UserBundle\Model\UserManager;
 use MyHealth\ApiBundle\Interfaces\ApiInterface;
+use MyHealth\SiteBundle\Entity\Measurement;
 use MyHealth\UserBundle\Entity\User;
 use Symfony\Component\Security\Core\Encoder\EncoderFactory;
 
-class UserService implements ApiInterface{
-
-	/**
-	 * @var \FOS\UserBundle\Model\UserManager
-	 */
-	protected $manager;
-
-	/**
-	 * @var \Symfony\Component\Security\Core\Encoder\EncoderFactory
-	 */
-	protected $factory;
+class MeasurementService implements ApiInterface{
 
 	/**
 	 * @var \Doctrine\ORM\EntityManager
@@ -26,22 +17,40 @@ class UserService implements ApiInterface{
 	protected $entity;
 
 	/**
-	 * @param UserManager $manager
-	 * @param EncoderFactory $factory
 	 * @param EntityManager $entity
-	 * @param Integer $loginAttempts
 	 */
-	public function __construct(UserManager $manager, EncoderFactory $factory, EntityManager $entity) {
-		$this->manager = $manager;
-		$this->factory = $factory;
+	public function __construct(EntityManager $entity) {
 		$this->entity = $entity;
-		//$this->loginAttempts = $loginAttempts;
+	}
+
+	public function createMeasurement($data) {
+		$measurement = new Measurement();
+		$measurement->setValue(doubleval($data['value']));
+		$measurement->setInsertDate(new \DateTime());
+		$measurement->setType($data['type']);
+		$measurement->setUser($this->entity->getRepository('MyHealthUserBundle:User')->find($data['user_id']));
+
+		$this->entity->persist($measurement);
+		$this->entity->flush();
+	}
+
+	public function deleteMeasurement($data) {
+		$measurement = $this->entity->getRepository('MyHealthSiteBundle:Measurement')->find($data['id']);
+		$this->entity->remove($measurement);
+		$this->entity->flush();
+	}
+
+	public function getMeasurements($data) {
+		$measured = array();
+		$measurements = $this->entity->getRepository('MyHealthSiteBundle:Measurement')->findBy(array('fk_user_id' => $data['user_id'], 'type' => $data['type']));
+		foreach($measurements as $measurement) {
+			$measured[] = array('id' => $measurement->getId(), 'value' => $measurement->getValue(), 'date' => $measurement->getInsertDate());
+		}
+		return $measured;
 	}
 
 	/**
-	 * @param $data
 	 * @return array
-	 * @throws \Exception
 	 */
 	public function getName() {
 		return 'measurement';
